ced6333d86223898a35482bc446be4e3003cbd94
Monson2002 <monsonreji2002@gmail.com>
2023-01-09 17:12:16 +0530

    9-1-23 5:12 Deleted import proptypes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,6 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export class Navbar extends Component {
-    static propTypes = {}
 
     render() {
         return (
