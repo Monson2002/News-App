5a84d628b184459caddbbc8a8378bb4235e63a85
Monson2002 <monsonreji2002@gmail.com>
2023-01-09 16:25:04 +0530

    9-11-23 4:25pm

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,9 +21,9 @@ export class Navbar extends Component {
 
                                     Menu open: "hidden", Menu closed: "block"
           --> */}
-                                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                    {/* <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                    </svg>
+                                    </svg> */}
                                     {/* <!--
                                     Icon when menu is open.
 
@@ -36,30 +36,33 @@ export class Navbar extends Component {
                                     </svg>
                                 </button>
                             </div>
-                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex flex-shrink-0 items-center">
+                            <div className="flex items-center justify-between w-full sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center sm:p-4">
                                     <img className="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
                                     <img className="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
                                 </div>
-                                <div className="hidden sm:ml-6 sm:block">
-                                    <div className="flex space-x-4">
-                                        {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                                        <a href="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
 
-                                        <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
-                                    </div>
+                                <div className="flex space-x-4 sm:p-4">
+                                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                                    <div className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">NEWS App</div>
+
+                                    {/* <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a> */}
                                 </div>
+
                             </div>
                         </div>
                     </div>
 
                     {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-                    <div className="sm:hidden" id="mobile-menu">
+                    <div className="hidden sm:hidden" id="mobile-menu">
                         <div className="space-y-1 px-2 pt-2 pb-3">
                             {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                            <a href="/" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+                            {/* <a href="/" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
 
-                            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+                            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a> */}
+
+                            <div className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">NEWS App</div>
+
                         </div>
                     </div>
                 </nav>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,17 +5,17 @@ export default class NewsItem extends Component {
     render() {
         let { title, description, urlToImage, url } = this.props;
         return (
-            <div>
+            <div className='m-2'>
                 <div className="m-4 max-w-sm rounded overflow-hidden shadow-lg">
                     <img className="w-full" src={urlToImage} alt="Sunset in the mountains" />
                     <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">{title}</div>
-                        <p className="text-gray-700 text-base">
+                        <div className="font-bold text-base sm:text-lg mb-2">{title}</div>
+                        <p className="text-gray-700 text-sm sm:text-base">
                             {description}...
                         </p>
                         <div className='p-4'>
                             <a href={url} target="_blank" rel="noreferrer" className="flex justify-center items-center">
-                                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white p-2 sm:py-2 sm:px-4 border border-blue-500 hover:border-transparent rounded">
                                     Read More
                                 </button>
                             </a>
@@ -26,3 +26,5 @@ export default class NewsItem extends Component {
         )
     }
 }
+
+
